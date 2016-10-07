4c5e13bfbab329cb59d2be505adb75659ce73799
Zegelink <supercomboooo@gmail.com>
2016-10-07 14:12:28 -0700

    modify recursion to tail-recursive and use g++ -O3 to handle large stack requirement

--- hw1/intersectCount.cpp
+++ hw1/intersectCount.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
 using namespace std;
 
-int is_intersect(int p1, int p2, int q1, int q2);
-int intersections(int index1, int index2, int* p, int* q, int size);
-
+int is_intersect(int q1, int q2);
+int intersections(int index1, int index2, int* q, int size, int& sum);
 
 int main(){
 
    ifstream file;
    file.open("input.txt");
-
    int size;
    file>>size;
+   if(size<2){
+      cout<<("the size of lines are smaller than 2")<<endl;
+      return 0;
+   }
 
+
+   //p is not really useful since we know it is in ascending order
    int* p = new int [size];
    int* q = new int [size];
 
@@ -26,9 +31,12 @@ int main(){
       file>>q[i];
       file.get();
    }
-    //size starts from 0, so minus 1. divide by 2 for repeating pairs.
-    int num_intersection_pairs = intersections(0,0,p,q,size-1)/2;
+
+    //size starts from 0, so minus 1.
+    int num_intersection_pairs = 0;
+    intersections(0,1,q,size-1,num_intersection_pairs);
     
+    //write result into file
     ofstream outputfile;
     outputfile.open("output.txt");
     outputfile<<num_intersection_pairs;
@@ -40,54 +48,36 @@ int main(){
    return 0;
 }
 
-int intersections(int index1, int index2, int* p, int* q, int size){
-    //cout<<index1<<"  "<<index2<<endl;
-    if(index1 == size){
-        if(index2 == size){
-            //same line, last compare
+int intersections(int index1, int index2, int* q, int size, int& sum){
+    //cout<<index1<<"  "<<index2<<" "<<sum<<endl;
+    sum += is_intersect(q[index1],q[index2]);
+    //base case: last two lines to compare
+    if(index1 == size-1){
+        if(index2 >= size){
+           //last line 
             return 0;
         }
         else{
-            return is_intersect(p[index1],p[index2],q[index1],q[index2])+
-            intersections(index1,index2+1,p,q,size);
+            return intersections(index1,index2+1,q,size,sum);
         }
     }
     else{
         if(index2 == size){
-            return is_intersect(p[index1],p[index2],q[index1],q[index2])+
-            intersections(index1+1,0,p,q,size);
+            return intersections(index1+1,index1+2,q,size,sum);
         }
         else{
-            return is_intersect(p[index1],p[index2],q[index1],q[index2])+
-            intersections(index1,index2+1,p,q,size);
+            return intersections(index1,index2+1,q,size,sum);
         }
     }
 }
 
 //a function that determines if two lines intersect
-int is_intersect(int p1, int p2, int q1, int q2){
-    if (p1 == p2){
-        //this indicate the same line for our problem
-        return 0;
-    }
-    if (q1 == q2){
-        //intersect at y = 1
+//precondition:p1<p2
+int is_intersect(int q1, int q2){
+    if (q1>=q2){
         return 1;
     }
-    if(q1>q2){
-        if(p1>p2){
-            return 0;
-        }
-        else{
-            return 1;
-        }
-    }
     else{
-        if(p1>p2){
-            return 1;
-        }
-        else{
-            return 0;
-        }
+       return 0;
     }
 }
