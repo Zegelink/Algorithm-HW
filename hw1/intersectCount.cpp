9f55a33705be2139c2ef85b62d6d6eaafd2dca7c
Zegelink <supercomboooo@gmail.com>
2016-10-18 12:42:54 -0700

    Same algorithm runs in O(n log n) time using divide and conquer

--- hw1/intersectCount.cpp
+++ hw1/intersectCount.cpp
@@ -1,10 +1,21 @@
+/*
+ Author: Chongxian Chen
+ DATE: October 11, 2016
+ */
+
 #include <iostream>
 #include <fstream>
-#include <unistd.h>
 using namespace std;
 
+//step1
 int is_intersect(int q1, int q2);
 int intersections(int index1, int index2, int* q, int size, int& sum);
+
+//step2
+int intersect(int* q, int start, int end);
+int group_intersect(int* q, int start, int m, int end);
+void merge(int*array, int start, int end, int m);
+
 
 int main(){
 
@@ -16,7 +27,6 @@ int main(){
       cout<<("the size of lines are smaller than 2")<<endl;
       return 0;
    }
-
 
    //p is not really useful since we know it is in ascending order
    int* p = new int [size];
@@ -34,7 +44,7 @@ int main(){
 
     //size starts from 0, so minus 1.
     int num_intersection_pairs = 0;
-    intersections(0,1,q,size-1,num_intersection_pairs);
+    num_intersection_pairs = intersect(q,0,size-1);
     
     //write result into file
     ofstream outputfile;
@@ -48,6 +58,81 @@ int main(){
    return 0;
 }
 
+
+//A recursive function that constantly divide the array into two until the array size is 1
+//and compute the intersection pairs by calling itself and group_intersect
+//returns the total intersection pairs in this group
+//Running time O(n log n)
+int intersect(int* q, int start, int end){
+    if ((end-start)>0){
+        int m = (end-start)/2+start;
+        int i1 = intersect(q, start,m);
+        int i2 = intersect(q,m+1,end);
+        int i3 = group_intersect(q,start,m,end);
+        return i1+i2+i3;
+    }
+    return 0;
+}
+
+//Compute the intersection pairs between two groups in linear time
+//by sorting two sub groups.
+int group_intersect(int* q, int start, int m, int end){
+    //sort two sub group
+    merge(q,start,m,(m-start)/2+start);
+    merge(q,m+1,end,(end-m-1)/2+m+1);
+    
+    int num_intersection_pairs = 0;
+    int i = end;
+    int j = m;
+    while( i>=m+1 && j>=start){
+        if(q[i]>q[j]){
+            i--;
+            //this line intersects with every line between (q[j], q[m]]
+            num_intersection_pairs+=(m-j);
+        }
+        else {
+            j--;
+        }
+        if(j<start){
+            //lines between [q[m+1],q[i]] intersects with every line
+            num_intersection_pairs+=(i-m)*(m-start+1);
+        }
+    }
+    return num_intersection_pairs;
+}
+
+//merge function. very similiar to the one used mergeSort algorithm
+void merge(int*array, int start, int end, int m){
+    int i = start, j = m+1;
+    //copy array
+    int *copy = new int [end+1];
+    for (int g = start; g <= end;g++){
+        copy[g] = array[g];
+    }
+    for(int k = start; k <= end; k++){
+        if (j>end){
+            array[k]=copy[i];
+            i++;
+        }
+        else if(i>m){
+            array[k]=copy[j];
+            j++;
+        }
+        else if(copy[i]<copy[j]){
+            array[k]=copy[i];
+            i++;
+        }
+        else{
+            array[k]=copy[j];
+            j++;
+        }
+    }
+    delete[] copy;
+}
+
+
+//Step 1 function
+//running time O(n^2)
 int intersections(int index1, int index2, int* q, int size, int& sum){
     //cout<<index1<<"  "<<index2<<" "<<sum<<endl;
     sum += is_intersect(q[index1],q[index2]);
